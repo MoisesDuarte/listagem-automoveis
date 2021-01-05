5987575ffbb07408d14de86d1ab23382dcb7e9f6
MoisesDuarte <moisesduartejunior@gmail.com>
2021-01-05 13:55:19 -0300

    Organizando folhas de estilo

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <Appbar />
-    <main class="ma-2">
-      <BrandsTable/>
+    <main class="container">
+      <BrandsTable @select-brand="selectedBrand = $event" />
     </main>
   </div>
 </template>
@@ -17,28 +17,16 @@ export default {
     Appbar,
     BrandsTable,
   },
+  data() {
+    return {
+      selectedBrand: null,
+    };
+  },
 }
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap');
-
-:root {
-  --color-primary: #25C98E;
-  --color-grey: #f7f8fb;
-  --color-blue: #4F73DF;
-  --text-black: #333333;
-}
-
-body {
-  background: var(--color-grey);
-  color: var(--text-black);
-  margin: 0;
-  padding: 0;
-  font-family: 'Comfortaa', cursive;
-}
-
-.ma-2 {
+.container {
   margin: 2em;
 }
 </style>
--- src/components/BrandsTable.vue
+++ src/components/BrandsTable.vue
@@ -7,7 +7,7 @@
         Marcas
       </header>
       <main class="card-content">
-        <table class="brands-table">
+        <table class="app-table">
           <tr>
             <th>Marca</th>
             <th></th>
@@ -17,7 +17,9 @@
               {{ brand.nome }}
             </td>
             <td>
-              <a role="button">Ver modelos</a>  
+              <a class="flat-button" role="button" @click="selectBrand(brand.codigo)">
+                Ver modelos
+              </a>  
             </td>
           </tr>
         </table>
@@ -44,6 +46,9 @@ export default {
           this.brands = res.data;
         });
     },
+    selectBrand(id) {
+      this.$emit('select-brand', id);
+    },
   },
   mounted() {
     this.fetchBrands();
@@ -54,53 +59,5 @@ export default {
 <style scoped>
 h1 {
   font-size: 1.5rem;
-}
-
-/* Card Styling */
-.card {
-  border: 1px solid #E3E6F0;
-  border-radius: 6px;
-  background: #ffffff;
-  box-shadow: 0px 0px 20px 0px rgba(58, 59, 69, .15);
-}
-
-.card-header {
-  border-bottom: 1px solid #E3E6F0;
-  border-top-left-radius: inherit;
-  border-top-right-radius: inherit;
-  background: #F8F9FC;
-  padding: 1.5rem;
-  font-weight: 700;
-  color: var(--color-blue);
-}
-
-.card-content {
-  border-bottom-left-radius: inherit;
-  border-bottom-right-radius: inherit;
-  background: #ffffff;
-  padding: 1.5rem;
-  padding-top: 2em;
-  max-height: 25em;
-  overflow-y: scroll;
-}
-
-/* Table Styling */
-.brands-table {
-  border-collapse: collapse;
-  width: 100%;
-}
-
-.brands-table tr {
-  border-bottom: 1px solid #E3E6F0;
-}
-
-.brands-table tr:first-child {
-  border-top: 1px solid #E3E6F0;
-  border-bottom: 2px solid #E3E6F0;
-}
-
-.brands-table th, td {
-  text-align: left;
-  padding: 1rem;
 }
 </style>
